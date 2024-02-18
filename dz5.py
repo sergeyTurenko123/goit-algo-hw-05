daa38c0afce7a92577759e15b09daf7c02a355c2
sergeyTurenko123 <sergej_petrovich_8484@ukr.net>
2024-02-18 19:22:10 +0300

    dz5

--- dz5.py
+++ dz5.py
@@ -26,16 +26,14 @@ getcontext().prec = 6
 
 def generator_numbers(text: str):
     numbers = re.findall(pattern= r"(\d+\.\d+)", string= text) # Знаходження цифр
-    for number in numbers: 
-        yield(number) # Вивід кожного числа окремо
+    for number in numbers:
+        yield(number ) # Вивід кожного числа окремо
 
 def sum_profit(text: str, operation: Callable):    # виклик программи generator_numbers
-    number = sum(map(Decimal, operation(text))) # Сумірування знайдених чисел
+    number = sum(map(Decimal, operation(text))) # Сумірування знайдених чисел. десятичне число
     return number
 
 text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
 total_income = sum_profit(text, generator_numbers) # Виклик функції sum_profit з результатом generator_numbers
 print(f"Загальний дохід: {total_income}")
-
-
 
---
+++ dz5_4.py
@@ -0,0 +1,139 @@
+def parse_input(user_input):
+    name, *args = user_input.split()
+    name = name.strip().lower()
+    return name, *args
+
+def input_error(func):
+    def inner(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except ValueError:
+            return "Give me name and phone please."
+    return inner
+
+@input_error
+def add_contact(args):
+    name, phone = args
+    if len(phone) == 10:
+            phone = f"+38{phone}"
+    elif len(phone) == 11:
+        phone = f"+3{phone}"
+    elif len(match) == 12:
+        phone = f"+{phone}"
+    else:
+        phone = phone
+    with open('name_phone.txt', "r", encoding = "utf-8") as fh:
+        lines = fh.readlines()
+    lists = {}
+    for line in lines:
+        list_line = line.split(",")
+        match = re.sub(pattern = r"[\s\D]", repl = r"", string = list_line[1])
+        lists.update({list_line[0]: f"+{match.strip()}"})
+    if name in lists:
+        return("Contact exists. Re-record?")
+    else:
+        lists[name] = phone
+        with open("name_phone.txt","w", encoding = "utf-8") as fh:
+            for key, value in lists.items():
+                fh.write(f'{key},{value}\n')
+        return "Contact added."
+
+@input_error
+def change_contact(args):
+    name, phone = args
+    if len(phone) == 10:
+        phone = f"+38{phone}"
+    elif len(phone) == 11:
+        phone = f"+3{phone}"
+    elif len(phone) == 12:
+        phone = f"+{phone}"
+    else:
+        phone = phone
+    with open('name_phone.txt', "r", encoding = "utf-8") as fh:
+        lines = fh.readlines()
+    lists = {}
+    for line in lines:
+        list_line = line.split(",")
+        match = re.sub(pattern = r"[\s\D]", repl = r"", string = list_line[1])
+        lists.update({list_line[0]: f"+{match.strip()}"})
+    if name in lists:
+        lists[name] = phone
+        with open("name_phone.txt","w", encoding = "utf-8") as fh:
+            for key, value in lists.items():
+                fh.write(f'{key},{value}\n')
+        return "Contact updated."
+    else:
+        print("Contact does not exist. Will you write it down?")
+    
+@input_error
+def show_phone(args):
+    [name] = args
+    with open('name_phone.txt', "r", encoding = "utf-8") as fh:
+        lines = fh.readlines()
+    lists = {}
+    for line in lines:
+        list_line = line.split(",")
+        pattern = r"[\s\D]"
+        repl = r""
+        match = re.sub(pattern, repl, list_line[1])
+        if len(match) == 10:
+            match = f"+38{match}"
+        elif len(match) == 11:
+            match = f"+3{match}"
+        elif len(match) == 12:
+            match = f"+{match}"
+        else:
+            match = match
+        lists.update({list_line[0]: match.strip()})
+    if name in lists:
+        return(f"{name},{lists[name]}")
+    else:
+        return("Contact does not exist. Will you write it down?")
+    
+import re
+@input_error
+def show_all():
+    with open('name_phone.txt', "r", encoding = "utf-8") as fh:
+        lines = fh.readlines()
+    lists = {}
+    for line in lines:
+        list_line = line.split(",")
+        pattern = r"[\s\D]"
+        repl = r""
+        match = re.sub(pattern, repl, list_line[1])
+        if len(match) == 10:
+            match = f"+38{match}"
+        elif len(match) == 11:
+            match = f"+3{match}"
+        elif len(match) == 12:
+            match = f"+{match}"
+        else:
+            match = match
+        lists.update({list_line[0]: match.strip()})
+    return lists
+
+def main():
+    contacts = {}
+    print("Welcome to the assistant bot!")
+    while True:
+        user_input = input("Enter a command: ")
+        command, *args = parse_input(user_input)
+
+        if command in ["close", "exit"]:
+            print("Good bye!")
+            break
+        elif command == "hello":
+            print("How can I help you?")
+        elif command == "add":
+            print(add_contact(args))
+        elif command == "change":
+            print(change_contact(args))
+        elif command == "phone":
+            print(show_phone(args))
+        elif command == "all":
+            print(show_all())
+        else:
+            print("Invalid command.")
+
+if __name__ == "__main__":
+    main()
